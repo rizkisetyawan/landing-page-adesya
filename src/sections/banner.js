28ada5ba3cd46119411aa92d0feaffdd3c2bcc22
rizki setyawan <rizki.delameta19@gmail.com>
2023-03-17 17:22:50 +0700

    prepare deploy

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
-import { jsx, Box, Container, Heading, Text, Button, Image } from 'theme-ui';
-import { Link as MenuLink } from 'react-scroll';
-import banner from 'assets/images/banner2.png';
+import { jsx, Box, Container, Heading, Text, Button, Image } from "theme-ui";
+import { Link as MenuLink } from "react-scroll";
+import banner from "assets/images/banner2.png";
 
 const Banner = () => {
   return (
@@ -9,15 +9,15 @@ const Banner = () => {
       <Container>
         <Box sx={styles.contentWrapper}>
           <Box sx={styles.content}>
-            <Heading sx={styles.title}>
-              Kontraktor Jasa dan Perdagangan
-            </Heading>
+            <Heading sx={styles.title}>Kontraktor Jasa dan Perdagangan</Heading>
             <Text as="p" sx={styles.text}>
-              Membantu anda dalam hal pekerjaan yang berhubungan segala jenis keperluan atau kebutuhan perkantoran.
-              Kami memiliki tenaga ahli yang terampil dan sudah berpengalaman untuk mendapatkan hasil yang terbaik.
+              Membantu anda dalam hal pekerjaan yang berhubungan segala jenis
+              keperluan atau kebutuhan perkantoran. Kami memiliki tenaga ahli
+              yang terampil dan sudah berpengalaman untuk mendapatkan hasil yang
+              terbaik.
             </Text>
             <MenuLink
-              to='services'
+              to="services"
               spy={true}
               offset={-70}
               smooth={true}
@@ -29,15 +29,19 @@ const Banner = () => {
                 Explore Now
               </Button>
             </MenuLink>
-             
-            {/* <Box sx={styles.clients}>
-              <Image src={paypal} alt="paypal" />
-              <Image src={google} alt="google" />
-              <Image src={dropbox} alt="dropbox" />
-            </Box> */}
           </Box>
           <Box sx={styles.illustration}>
             <Image src={banner} alt="banner" />
+            <Box sx={styles.contentImage}>
+              <Heading sx={styles.titleImage}>
+                CV. ADESYA TECH INVESTMENT
+              </Heading>
+              <Text as="p" sx={styles.textImage}>
+                Best Partner For Your Company
+              </Text>
+            </Box>
+            <Box sx={styles.borderImage} />
+            <Box sx={styles.blackOpacity} />
           </Box>
         </Box>
       </Container>
@@ -49,54 +53,100 @@ export default Banner;
 
 const styles = {
   contentWrapper: {
-    display: [null, null, null, 'flex', 'grid'],
-    gridTemplateColumns: 'repeat(2, 1fr)',
-    alignItems: 'center',
-    justifyContent: 'center',
-    minHeight: [null, null, null, null, null, '100vh'],
+    display: [null, null, null, "flex", "grid"],
+    gridTemplateColumns: "repeat(2, 1fr)",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: [null, null, null, null, null, "100vh"],
     pt: [100, null, null, 120, 130, 120, 0],
   },
   content: {
     maxWidth: [507, null, null, 324, 450],
   },
   title: {
-    fontWeight: 'bold',
-    fontSize: ['30px', null, null, null, '42px', '40px', '52px'],
+    fontWeight: "bold",
+    fontSize: ["30px", null, null, null, "42px", "40px", "52px"],
     lineHeight: 1.33,
-    letterSpacing: '-1px',
-    color: 'textSecondary',
+    letterSpacing: "-1px",
+    color: "textSecondary",
+  },
+  titleImage: {
+    fontWeight: "bold",
+    fontSize: ["18px", null, null, null, "20px", "20px", "24px"],
+    lineHeight: 1.33,
+    textDecoration: "underline",
+    color: "white",
   },
   text: {
-    fontSize: ['14px', '14px', '14px', '16px', '16px', '18px'],
+    fontSize: ["14px", "14px", "14px", "16px", "16px", "18px"],
     lineHeight: [1.85, 1.85, 1.85, 1.85, 1.85, 2.33],
-    color: 'textSecondary',
-    mt: ['14px', '19px'],
+    color: "textSecondary",
+    mt: ["14px", "19px"],
+  },
+  textImage: {
+    fontSize: ["14px", "14px", "14px", "16px", "16px", "18px"],
+    lineHeight: [1.85, 1.85, 1.85, 1.85, 1.85, 2.33],
+    color: "white",
   },
   button: {
-    display: ['none', 'flex'],
+    display: ["none", "flex"],
     mt: [45, 45, 45, 25, 25],
   },
   clients: {
-    display: 'flex',
-    alignItems: 'center',
-    mt: ['20px', '45px', '45px', '30px', '45px'],
+    display: "flex",
+    alignItems: "center",
+    mt: ["20px", "45px", "45px", "30px", "45px"],
     img: {
-      maxWidth: ['80px', '100%', '100%', '100%'],
-      '+ img': {
-        ml: ['14px', '28px', '28px', '20px'],
+      maxWidth: ["80px", "100%", "100%", "100%"],
+      "+ img": {
+        ml: ["14px", "28px", "28px", "20px"],
       },
     },
   },
   illustration: {
-    display: ['block', 'block'],
-    mt: ['30px', '30px', 0],
-    mb: ['60px', '60px', 0],
+    position: "relative",
+    display: ["block", "block"],
+    mt: ["30px", "30px", 0],
+    mb: ["60px", "60px", 0],
     img: {
-      width: '100%',
-      maxWidth: ['100%', '100%', '100%', '100%', '90%', '90%', '100%'],
-      borderRadius: '18px',
-      maxHeight: ['350px', '500px'],
-      objectFit: 'cover'
+      width: "90%",
+      borderRadius: "18px",
+      maxHeight: ["350px", "500px"],
+      objectFit: "cover",
+      position: "relative",
+      zIndex: 1,
     },
+    position: "relative",
+  },
+  borderImage: {
+    width: "90%",
+    height: "100%",
+    position: "absolute",
+    border: "1px solid",
+    borderColor: "primary",
+    top: 32,
+    right: 16,
+    borderRadius: "18px",
+    maxHeight: ["350px", "500px"],
+  },
+  blackOpacity: {
+    width: "90%",
+    height: "100%",
+    position: "absolute",
+    backgroundColor: "#000",
+    opacity: 0.5,
+    zIndex: 1,
+    top: 0,
+    borderRadius: "18px",
+    maxHeight: ["350px", "500px"],
+  },
+  contentImage: {
+    width: "380px",
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%,-50%)",
+    zIndex: 2,
+    maxHeight: ["350px", "500px"],
   },
 };
--- src/sections/projects.js
+++ src/sections/projects.js
@@ -1,54 +1,54 @@
 /** @jsx jsx */
-import { jsx, Box, Container, Image, Button } from 'theme-ui';
-import { RiArrowRightSLine } from 'react-icons/ri';
-import Masonry from 'react-masonry-component';
-import SectionHeading from 'components/section-heading';
-import GalleryCard from 'components/cards/gallery-card';
+import { jsx, Box, Container, Image, Button } from "theme-ui";
 
-import gallery1 from 'assets/images/gallery/project-1.png';
-import gallery2 from 'assets/images/gallery/project-2.png';
-import gallery3 from 'assets/images/gallery/project-3.png';
-import gallery4 from 'assets/images/gallery/project-4.png';
-import gallery5 from 'assets/images/gallery/project-5.png';
-import gallery6 from 'assets/images/gallery/project-6.png';
-import gallery7 from 'assets/images/gallery/project-7.png';
-import gallery8 from 'assets/images/gallery/project-8.png';
-import gallery9 from 'assets/images/gallery/project-9.png';
-import gallery10 from 'assets/images/gallery/project-10.png';
-import gallery11 from 'assets/images/gallery/project-11.png';
-import gallery12 from 'assets/images/gallery/project-12.png';
+import Masonry from "react-masonry-component";
+import SectionHeading from "components/section-heading";
+import GalleryCard from "components/cards/gallery-card";
+
+import gallery1 from "assets/images/gallery/project-1.png";
+import gallery2 from "assets/images/gallery/project-2.png";
+import gallery3 from "assets/images/gallery/project-3.png";
+import gallery4 from "assets/images/gallery/project-4.png";
+import gallery5 from "assets/images/gallery/project-5.png";
+import gallery6 from "assets/images/gallery/project-6.png";
+import gallery7 from "assets/images/gallery/project-7.png";
+import gallery8 from "assets/images/gallery/project-8.png";
+import gallery9 from "assets/images/gallery/project-9.png";
+import gallery10 from "assets/images/gallery/project-10.png";
+import gallery11 from "assets/images/gallery/project-11.png";
+import gallery12 from "assets/images/gallery/project-12.png";
 
 const data = [
   {
     id: 6,
     image: gallery6,
-    title: 'Kitchen room',
+    title: "Kitchen room",
   },
   {
     id: 2,
     image: gallery2,
-    title: 'Meeting corner',
+    title: "Meeting corner",
   },
   {
     id: 3,
     image: gallery3,
-    title: 'Guest meeting room',
+    title: "Guest meeting room",
   },
   {
     id: 4,
     image: gallery4,
-    title: 'Guest rest room',
+    title: "Guest rest room",
   },
-  
+
   {
     id: 5,
     image: gallery5,
-    title: 'Single working space',
+    title: "Single working space",
   },
   {
     id: 1,
     image: gallery1,
-    title: 'Front working space',
+    title: "Front working space",
   },
 ];
 
@@ -56,32 +56,32 @@ const moreData = [
   {
     id: 7,
     image: gallery7,
-    title: 'Single working space',
+    title: "Single working space",
   },
   {
     id: 8,
     image: gallery8,
-    title: 'Single working space',
+    title: "Single working space",
   },
   {
     id: 9,
     image: gallery9,
-    title: 'Single working space',
+    title: "Single working space",
   },
   {
     id: 10,
     image: gallery10,
-    title: 'Single working space',
+    title: "Single working space",
   },
   {
     id: 11,
     image: gallery11,
-    title: 'Single working space',
+    title: "Single working space",
   },
   {
     id: 12,
     image: gallery12,
-    title: 'Single working space',
+    title: "Single working space",
   },
 ];
 
@@ -90,15 +90,15 @@ const masonryOptions = {
 };
 
 const Projects = () => {
-  const [projects, setProjects] = React.useState(data)
+  const [projects, setProjects] = React.useState(data);
 
   const handleButton = () => {
     if (projects.length === 6) {
-      setProjects([...data, ...moreData])
+      setProjects([...data, ...moreData]);
     } else {
-      setProjects(data)
+      setProjects(data);
     }
-  }
+  };
 
   return (
     <Box id="projects" as="section" sx={styles.section}>
@@ -113,9 +113,9 @@ const Projects = () => {
             <GalleryCard key={item.id} item={item} />
           ))}
         </Box>
+
         <Button variant="muted" sx={styles.button} onClick={handleButton}>
-          {projects.length === 6 ? 'Explore More' : 'Hide'}
-          
+          {projects.length === 6 ? "Explore More" : "Hide"}
         </Button>
       </Container>
     </Box>
@@ -133,18 +133,18 @@ const styles = {
     mb: [30, 30, 40, 60],
   },
   galleryWrapper: {
-    mx: '-15px',
+    mx: "-15px",
   },
   button: {
     minHeight: [50, 50, 50, 60],
     fontSize: [14, 14, 16],
-    width: '100%',
+    width: "100%",
     svg: {
-      transition: 'margin-left 0.3s ease-in-out 0s',
+      transition: "margin-left 0.3s ease-in-out 0s",
     },
-    ':hover': {
+    ":hover": {
       svg: {
-        ml: '5px',
+        ml: "5px",
       },
     },
   },
