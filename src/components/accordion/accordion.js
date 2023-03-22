5ea67a61a400db2a9f4cb1a4983d674fa5d3486c
rizki setyawan <rizki.delameta19@gmail.com>
2023-03-22 09:19:20 +0700

    setup accordion

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -1,45 +1,30 @@
 import React from "react";
 import { Image } from "theme-ui";
-import { BaseAccordion } from "./base-accordion";
 import accordionIconClose from "assets/accordion-arrow.svg";
 import accordionIconOpen from "assets/accordion-arrow-e.svg";
-import {
-  AccordionButton,
-  AccordionItem,
-  AccordionContents,
-  single,
-  preventClose,
-  combineReducers,
-} from "./shared";
+import { AccordionButton, AccordionItem, AccordionContents } from "./shared";
 
-export default function Accordion({ items, ...props }) {
+function Item({ data }) {
+  const [open, setOpen] = React.useState(false);
   const openIcon = <Image src={accordionIconOpen} alt="open icon" />;
   const closeIcon = <Image src={accordionIconClose} alt="close icon" />;
   return (
-    <BaseAccordion
-      stateReducer={combineReducers(single, preventClose)}
-      {...props}
-    >
-      {({ openIndexes, handleItemClick }) => (
-        <>
-          {items.map((item, index) => (
-            <AccordionItem
-              key={item.title}
-              isOpen={openIndexes.includes(index)}
-            >
-              <AccordionButton onClick={() => handleItemClick(index)}>
-                <span>
-                  {openIndexes.includes(index) ? openIcon : closeIcon}
-                </span>
-                {item.title}
-              </AccordionButton>
-              <AccordionContents isOpen={openIndexes.includes(index)}>
-                {item.contents}
-              </AccordionContents>
-            </AccordionItem>
-          ))}
-        </>
-      )}
-    </BaseAccordion>
+    <AccordionItem key={data.title} isOpen={open}>
+      <AccordionButton onClick={() => setOpen((prevState) => !prevState)}>
+        <span>{open ? openIcon : closeIcon}</span>
+        {data.title}
+      </AccordionButton>
+      <AccordionContents isOpen={open}>{data.contents}</AccordionContents>
+    </AccordionItem>
+  );
+}
+
+export default function Accordion({ items }) {
+  return (
+    <>
+      {items.map((item) => (
+        <Item data={item} key={item.title} />
+      ))}
+    </>
   );
 }
--- src/components/accordion/shared.js
+++ src/components/accordion/shared.js
@@ -105,18 +105,3 @@ export const AccordionItem = ({ isOpen, children, ...rest }) => (
     {children}
   </div>
 );
-
-export const preventClose = (state, changes) =>
-  changes.type === "closing" && state.openIndexes.length < 2
-    ? { ...changes, openIndexes: state.openIndexes }
-    : changes;
-
-export const single = (state, changes) =>
-  changes.type === "opening"
-    ? { ...changes, openIndexes: changes.openIndexes.slice(-1) }
-    : changes;
-
-export const combineReducers =
-  (...reducers) =>
-  (state, changes) =>
-    reducers.reduce((acc, reducer) => reducer(state, acc), changes);
